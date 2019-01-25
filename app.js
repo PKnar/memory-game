05ec75449e153740b9622e9e0870599f6913c03d
Knar <qenpoghos@gmail.com>
2019-01-25 19:01:04 +0100

    added few functions,to start and stop timer also reload the game on button click

--- app.js
+++ app.js
@@ -1,94 +1,115 @@
-"use strict";
-const container = document.getElementById("container");
+const memoryGame = (() => {
+  "use strict";
+  const container = document.getElementById("container");
+  const winnerDiv = document.getElementById("winnerDiv");
+  const button = document.getElementById("restart");
+  const timerP = document.getElementById("timer");
 
-let listOfColor = [
-  "white",
-  "purple",
-  "blue",
-  "brown",
-  "green",
-  "orange",
-  "white",
-  "green",
-  "orange",
-  "purple",
-  "brown",
-  "blue"
-];
+  let firstClick = false;
+  let firstCard, secondCard;
+  let wait = false;
+  let timer = 0;
+  let correctAnswerCounter = 0;
 
-/*Randomizing the list at first then creating the cards,
-to make sure every every card has a pair.
- */
-listOfColor.sort(() => 0.5 - Math.random());
+  let listOfColor = [
+    "white",
+    "purple",
+    "blue",
+    "brown",
+    "green",
+    "orange",
+    "white",
+    "green",
+    "orange",
+    "purple",
+    "brown",
+    "blue"
+  ];
 
-listOfColor.forEach(color => {
-  const cardWrapper = document.createElement("div");
-  const frontCard = document.createElement("div");
-  const backCard = document.createElement("div");
+  /*Randomizing the list at first then creating the cards,
+    to make sure every every card has a pair.
+     */
+  listOfColor.sort(() => 0.5 - Math.random());
 
-  cardWrapper.className = "memory-card";
-  frontCard.className = `front-card ${color}`;
-  backCard.className = "back-card gray";
-  frontCard.style.backgroundColor = `${color}`;
+  listOfColor.forEach(color => {
+    const cardWrapper = document.createElement("div");
+    const frontCard = document.createElement("div");
+    const backCard = document.createElement("div");
 
-  cardWrapper.dataset.name = color;
+    cardWrapper.className = "memory-card";
+    frontCard.className = `front-card ${color}`;
+    backCard.className = "back-card gray";
+    frontCard.style.backgroundColor = `${color}`;
 
-  cardWrapper.appendChild(backCard);
-  cardWrapper.appendChild(frontCard);
-  container.appendChild(cardWrapper);
-});
+    cardWrapper.dataset.name = color;
 
-let firstClick = false;
-let firstCard, secondCard;
-let wait = false;
-let timer = 0;
-let timerP = document.getElementById("timer");
-const cards = document.querySelectorAll(".memory-card");
+    cardWrapper.appendChild(backCard);
+    cardWrapper.appendChild(frontCard);
+    container.appendChild(cardWrapper);
+  });
 
-cards.forEach(card => card.addEventListener("click", toggleRotateClass));
+  /* add event listener to each card */
+  const cards = document.querySelectorAll(".memory-card");
+  cards.forEach(card => card.addEventListener("click", toggleRotateClass));
 
-function toggleRotateClass() {
-  if (wait) return;
-  if (this === firstCard) return;
+  function toggleRotateClass() {
+    if (wait) return;
+    if (this === firstCard) return;
 
-  this.classList.toggle("rotate");
+    this.classList.toggle("rotate");
 
-  if (!firstClick) {
-    firstClick = true;
-    firstCard = this;
-  } else {
-    firstClick = false;
-    secondCard = this;
-    checkForMatchingCards();
+    if (!firstClick) {
+      firstClick = true;
+      firstCard = this;
+    } else {
+      firstClick = false;
+      secondCard = this;
+      checkForMatchingCards();
+    }
   }
-}
 
-setInterval(function start() {
-  timer++;
-  return (timerP.innerHTML = "your time is " + timer);
-}, 1000);
-
-function checkForMatchingCards() {
-  if (firstCard.dataset.name === secondCard.dataset.name) {
-    removeClickEvent() && removeToggleClass();
-    ++correctAnswerCounter;
-    console.log(correctAnswerCounter);
-    return;
-  }
-  return removeToggleClass();
-}
-
-//if cards are matching, turning cards into non-clickable objects
-function removeClickEvent() {
-  firstCard.removeEventListener("click", toggleRotateClass);
-  secondCard.removeEventListener("click", toggleRotateClass);
-}
-
-function removeToggleClass() {
-  wait = true;
-  setTimeout(() => {
-    firstCard.classList.remove("rotate");
-    secondCard.classList.remove("rotate");
-    wait = false;
+  const startTimer = setInterval(() => {
+    timer++;
+    return (timerP.innerHTML = "Your time is " + timer);
   }, 1000);
-}
+
+  const stopTimer = () => {
+    clearInterval(startTimer);
+  };
+
+  function checkForMatchingCards() {
+    if (firstCard.dataset.name === secondCard.dataset.name) {
+      removeClickEvent() && removeRotateClass();
+      correctAnswerCounter++;
+      if (correctAnswerCounter === listOfColor.length / 2) {
+        stopTimer();
+        winnerDiv.style.display = "block";
+      }
+      return;
+    }
+    return removeRotateClass();
+  }
+
+  //if cards are matching, turning cards into non-clickable objects
+  function removeClickEvent() {
+    firstCard.removeEventListener("click", toggleRotateClass);
+    secondCard.removeEventListener("click", toggleRotateClass);
+  }
+
+  function removeRotateClass() {
+    wait = true;
+    setTimeout(() => {
+      firstCard.classList.remove("rotate");
+      secondCard.classList.remove("rotate");
+      wait = false;
+    }, 1000);
+  }
+
+  //click the button to reload the game
+  const reload = () => {
+    document.location.reload();
+  };
+  button.addEventListener("click", reload);
+})();
+
+document.addEventListener("load", memoryGame);
